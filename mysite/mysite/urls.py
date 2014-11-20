fd56bdfd9d50d18c220cd96f5ddb3730406f3b79
EricSchles <ericschles@gmail.com>
2014-11-20 06:49:33 -0800

    updating things - almost done with forms

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
+    '',
+    url(r'^forms/', include('forms.urls', namespace="forms")),
     url(r'^admin/', include(admin.site.urls)),
 )
